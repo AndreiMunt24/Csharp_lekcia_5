5b167e33f26a47450f2be82eebe033ab1f8b2274
Andrei Munteanu <muntyanua@internet.ru>
2024-07-09 20:28:32 +0300

    seminar5task2

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+void Main()
+{
+    int number = ReadInt("Введите число: ");
+    PrintNumber(number);
+}
+
+void PrintNumber(int N)
+{
+    if(N < 1) return;
+    System.Console.Write(N + " ");
+    PrintNumber(N - 1);    
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
